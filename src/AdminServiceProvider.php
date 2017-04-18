77018c7d55481a3bedea5bb5989d252f3bab0400
David Angulo <david.angulo.arias@gmail.com>
2017-04-17 23:08:10 -0500

    Create service provider for package

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace VerumConsilium\Admin;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{	
+	/**
+	 * Bootstrap the application services.
+	 * 
+	 * @return void
+	 */
+	public function boot()
+	{
+		
+	}
+
+	 /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+}
